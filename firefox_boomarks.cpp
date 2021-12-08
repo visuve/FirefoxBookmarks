ae742a49ab113a433e382677ab3ccde6adc5875c
visuve <veli-matti.visuri@iki.fi>
2021-12-08 15:40:00 +0200

    Small code improvements

--- firefox_boomarks.cpp
+++ firefox_boomarks.cpp
@@ -4,6 +4,47 @@
 #include <fstream>
 #include <filesystem>
 
+std::string uncompress(std::ifstream& stream, int compressed_size, int uncompressed_size)
+{
+	std::string compressed(compressed_size, '\0');
+	stream.read(compressed.data(), compressed_size);
+
+	std::string uncompressed(uncompressed_size, '\0');
+
+	int bytes_decompressed = LZ4_decompress_safe_partial(
+		compressed.data(),
+		uncompressed.data(),
+		compressed_size,
+		uncompressed_size,
+		uncompressed_size);
+
+	if (bytes_decompressed <= 0)
+	{
+		return {};
+	}
+
+	if (bytes_decompressed < uncompressed_size)
+	{
+		uncompressed.resize(bytes_decompressed);
+	}
+
+	return uncompressed;
+}
+
+int uncompressed_size_bytes(std::ifstream& stream)
+{
+	int result = 0;
+	stream.read(reinterpret_cast<char*>(&result), 4);
+	return result;
+}
+
+bool has_mozilla_lz4_header(std::ifstream& stream)
+{
+	std::string header(8, 0);
+	stream.read(header.data(), 8);
+	return header.starts_with("mozLz40");
+}
+
 int main(int argc, char** argv)
 {
 	if (argc != 3)
@@ -17,9 +58,9 @@ int main(int argc, char** argv)
 	std::filesystem::path input_path(argv[1]);
 	std::filesystem::path output_path(argv[2]);
 
-	int file_size = static_cast<int>(std::filesystem::file_size(input_path)) - 12;
+	int compressed_size = static_cast<int>(std::filesystem::file_size(input_path)) - 12;
 
-	if (file_size <= 0)
+	if (compressed_size <= 0)
 	{
 		std::cerr << "The input file appears empty!" << std::endl;
 		return -2;
@@ -27,44 +68,29 @@ int main(int argc, char** argv)
 
 	std::ifstream input_file(input_path, std::ifstream::binary);
 
-	std::string header(8, 0);
-	input_file.read(header.data(), 8);
-
-	if (!std::string_view(header).starts_with("mozLz40"))
+	if (!has_mozilla_lz4_header(input_file))
 	{
 		std::cerr << "The input file does not have a Mozilla header!" << std::endl;
 		return -3;
 	}
 
-	uint32_t space_required = 0;
-	input_file.read(reinterpret_cast<char*>(&space_required), 4);
+	int uncompressed_size = uncompressed_size_bytes(input_file);
 
-	if (!space_required)
+	if (uncompressed_size <= 0)
 	{
-		std::cerr << "Failed to process bytes needed to uncompress!" << std::endl;
+		std::cerr << "Failed to read bytes needed to uncompress!" << std::endl;
 		return -4;
 	}
 
-	std::string compressed(file_size, '\0');
-	input_file.read(compressed.data(), file_size);
+	std::string uncompressed = uncompress(input_file, compressed_size, uncompressed_size);
 
-	std::string uncompressed(space_required, '\0');
-
-	int bytes_decoded = LZ4_decompress_safe_partial(
-		compressed.data(),
-		uncompressed.data(),
-		file_size,
-		space_required,
-		space_required);
-
-	if (bytes_decoded <= 0)
+	if (uncompressed.empty())
 	{
 		std::cerr << "LZ4_decompress_safe_partial failed!" << std::endl;
 		return -5;
 	}
 
 	std::ofstream output_file(output_path);
-	output_file.write(uncompressed.data(), static_cast<size_t>(bytes_decoded));
 
-	return 0;
+	return output_file.write(uncompressed.data(), uncompressed.size()) ? 0 : -6;
 }
